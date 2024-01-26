8f9a7e9749a6030c61c11942c34addf4ac1b1fb4
Ekram Ahmad <ekramahmad560@gmail.com>
2024-01-26 17:24:49 +0530

    Create aboutMe and contact Page

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,36 +1,44 @@
-import { Alert, AlertTitle, Button, ButtonGroup, Container, List, ListItem, ListItemText, Typography } from "@mui/material";
-import React, { useState } from 'react';
-import agent from "../../app/api/agent";
-export default function AboutPage(){
-  const [validationErrors,setValidationErrors]=useState<string[]>([]);
+import React from 'react';
+import { Typography, Card, CardContent, CardMedia, Grid } from '@mui/material';
 
-  function getValidationError(){
-    agent.TestErrors.getValidationError()
-    .then(()=> console.log('should not see this'))
-    .catch(error=>setValidationErrors(error));
-  }
-    return(
-      <Container>
-        <Typography gutterBottom variant="h2">Errors for testing</Typography>
-        <ButtonGroup fullWidth>
-            <Button variant="contained" onClick={()=>agent.TestErrors.get400Error().catch(error=> console.log(error))}>test 400 Error</Button>
-            <Button variant="contained" onClick={()=>agent.TestErrors.get401Error().catch(error=> console.log(error))}>test 401 Error</Button>
-            <Button variant="contained" onClick={()=>agent.TestErrors.get404Error().catch(error=> console.log(error))}>test 404 Error</Button>
-            <Button variant="contained" onClick={()=>agent.TestErrors.get500Error().catch(error=> console.log(error))}>test 500 Error</Button>
-            <Button variant="contained" onClick={getValidationError}>test Validation Error</Button>
-        </ButtonGroup>
-        {validationErrors.length>0 &&
-            <Alert severity="error">
-              <AlertTitle>Validation Errors</AlertTitle>
-              <List>
-                {validationErrors.map(error=>(
-                  <ListItem key={error}>
-                    <ListItemText>{error}</ListItemText>
-                  </ListItem>
-                ))}
-              </List>
-            </Alert>
-        }
-      </Container>
-    )
-}
+const AboutPage = () => {
+  return (
+    <div>
+      <Typography variant="h3" align="center" gutterBottom>
+        About Re-Store
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardMedia
+              component="img"
+              height="300"
+              alt="Re-Store Logo"
+              src='https://i.pinimg.com/736x/b2/7b/cb/b27bcb31c8dfc764da561147c3784b83.jpg'
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Welcome to Re-Store!
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Re-Store is your one-stop destination for all your shopping needs. We pride ourselves on offering a wide range of high-quality products, from electronics to fashion, at competitive prices.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Our mission is to provide a seamless and enjoyable shopping experience for our customers. With a user-friendly website and a secure checkout process, we strive to make your online shopping experience hassle-free.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Thank you for choosing Re-Store. We look forward to serving you and making your shopping experience exceptional.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default AboutPage;
--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,24 +1,76 @@
-import { Button, ButtonGroup, Typography } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { decrement, increment } from "./counterSlice";
 import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem, TextField, Button } from '@mui/material';
+import { Typography, List, ListItem, ListItemText, Link } from '@mui/material';
+
 
 export default function ContactPage() {
-    const dispatch = useAppDispatch();
-    const { data, title } = useAppSelector(state => state.counter);
     return (
         <>
-            <Typography gutterBottom variant='h3'>
-                {title}
-            </Typography>
-            <Typography variant='h5'>
-                The Data is: {data}
-            </Typography>
-            <ButtonGroup>
-                <Button onClick={() => dispatch(decrement(1))} variant='contained' color='error'>Decrement</Button>
-                <Button onClick={() => dispatch(increment(1))} variant='contained' color='secondary'>Increment</Button>
-                <Button onClick={() => dispatch(increment(5))} variant='contained' color='primary'>Increment by 5</Button>
-            </ButtonGroup>
+            <div>
+            <Typography variant="h3">Reach Us At</Typography>
+            <List sx={{ marginLeft: 10 }}>
+                <ListItem>
+                <ListItemText primary="+91-8601042544" />
+                </ListItem>
+                <ListItem>
+                <ListItemText
+                    primary={
+                    <Link href="mailto:ekramahmad560@gmail.com">
+                        Email: ekramahmad560@gmail.com
+                    </Link>
+                    }
+                />
+                </ListItem>
+                <ListItem>
+                <ListItemText
+                    primary="Address: A-143, 9th Floor, Sector-136, Noida, Uttar Pradesh"
+                />
+                </ListItem>
+            </List>
+            </div>
+            <form>
+      <p>Feedback and Queries</p>
+      <div>
+        <FormControl fullWidth sx={{ marginBottom: 2 }}>
+          <InputLabel>Select Issue*</InputLabel>
+          <Select>
+            <MenuItem value="">
+              -- Select Your Query --
+            </MenuItem>
+            <MenuItem value="Feedback">Feedback</MenuItem>
+            <MenuItem value="Product Related Queries">Product Related Queries</MenuItem>
+            <MenuItem value="Payment Related Issue">Payment Related Issue</MenuItem>
+            <MenuItem value="Delivery Related Queries">Delivery Related Queries</MenuItem>
+            <MenuItem value="Advertise With Us">Advertise With Us</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          label="Email Address*"
+          type="email"
+          placeholder="ekramahmad560@gmail.com"
+          fullWidth
+          sx={{ marginBottom: 2 }}
+        />
+        <TextField
+          label="Contact No."
+          type="text"
+          placeholder="1324567890"
+          fullWidth
+          sx={{ marginBottom: 2 }}
+        />
+        <TextField
+          label="Drop Your Query"
+          placeholder="Max Allowed Characters: 300"
+          multiline
+          rows={4}
+          fullWidth
+          sx={{ marginBottom: 2 }}
+        />
+        <Button variant="contained" type="button">
+          Submit
+        </Button>
+      </div>
+    </form>
         </>
 
     )
